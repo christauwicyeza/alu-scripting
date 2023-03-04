2ea531a7ec63826be2d644c2fa22b9b8ea99f223
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2023-03-04 20:29:58 +0200

    ss

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,16 +5,22 @@
 import json
 import requests
 
-import requests
-
 def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    :param subreddit: The name of the subreddit to query.
+    :return: The number of subscribers for the given subreddit, or 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    else:
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # raise an exception if status code is not 200
+        data = response.json()['data']
+        return data['subscribers']
+    except (requests.exceptions.HTTPError, json.JSONDecodeError):
         return 0
- 
 
 
